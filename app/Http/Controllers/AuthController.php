d24aa5799e6edceb88ee8f2b6108815c40a032d4
Rosila <87629081+RosilaRosila@users.noreply.github.com>
2024-06-04 02:11:38 +0700

    First Commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+
+    /* ----------- CONTROLLER LOGIN -----------*/
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function login_proses(Request $request)
+    {
+        Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ])->validate();
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            throw ValidationException::withMessages([
+                'email' => trans('auth.failed')
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        // if (auth()->user()->type == 'admin') {
+        //     return redirect()->route('admin/home');
+        // } else {
+        //     return redirect()->route('home');
+        // }
+
+        return redirect()->route('dashboard');
+    }
+
+
+    /* ----------- CONTROLLER REGISTER -----------*/
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function register_proses(Request $request)
+    {
+        Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ])->validate();
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('login');
+
+        // if (Auth::attempt($login)) {
+        //     return redirect()->route('admin.dashboard');
+        // } else {
+        //     return redirect()->route('login')->with('failed', 'Email atau Password Salah');
+        // }
+    }
+}
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin = Role::updateOrCreate(
+            [
+                'name' => 'admin',
+            ],
+            ['name' => 'admin']
+        );
+
+        $role_ptiket = Role::updateOrCreate(
+            [
+                'name' => 'ptiket',
+            ],
+            ['name' => 'ptiket']
+        );
+
+        $permission = Permission::updateOrCreate(
+            [
+                'name' => 'view_dashboard',
+            ],
+            ['name' => 'view_dashboard']
+        );
+
+        $permission2 = Permission::updateOrCreate(
+            [
+                'name' => 'view_home',
+            ],
+            ['name' => 'view_home']
+        );
+
+        $role_admin->givePermissionTo($permission);
+        $role_ptiket->givePermissionTo($permission2);
+
+        $user = User::find(1);
+
+        $user->assignRole('admin');
+    }
+}

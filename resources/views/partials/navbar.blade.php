8235d86b3a20ecab248eb82276a9ac00edcc025f
Rosila <87629081+RosilaRosila@users.noreply.github.com>
2024-06-14 00:28:36 +0700

    views update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -11,10 +11,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item ">
-                <a class="nav-link {{ ($title === "DASHBOARD") ? 'active' : '' }}" href="http://127.0.0.1:8000/Dashboard">Home</a>
+                <a class="nav-link {{ ($title === "DASHBOARD") ? 'active' : '' }}" href="http://127.0.0.1:8000/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ ($title === "INFO WISATA") ? 'active' : '' }}"" href=" http://127.0.0.1:8000/Info-Wisata">Info Wisata</a>
+                <a class="nav-link {{ ($title === "INFO WISATA") ? 'active' : '' }}"" href=" http://127.0.0.1:8000/info-wisata">Info Wisata</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Pesan Tiket</a>

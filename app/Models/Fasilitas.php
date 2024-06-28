791545a3397729c2f4451e2d4a679968f4dc2ecb
Rosila <87629081+RosilaRosila@users.noreply.github.com>
2024-06-29 00:56:01 +0700

    models update

--- app/Models/Fasilitas.php
+++ app/Models/Fasilitas.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Fasilitas extends Model
 {
     use HasFactory;
+
+    protected $table = 'fasilitas';
+    protected $primarykey = 'id';
+    protected $fillable = ['namaicon', 'imgicon'];
+
+    public $incrementing = false;
+    public $timestamps = true;
 }

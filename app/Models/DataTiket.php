6e324991f232a089fa86c191fbf03a36aba992e0
Rosila <87629081+RosilaRosila@users.noreply.github.com>
2024-07-01 16:06:18 +0700

    models new

--- app/Models/DataTiket.php
+++ app/Models/DataTiket.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class DataTiket extends Model
 {
     use HasFactory;
+
+    protected $table = 'data_tikets';
+    protected $primarykey = 'id';
+    protected $fillable = ['namawst', 'harga'];
+
+    public $incrementing = false;
+    public $timestamps = true;
 }
